9b9177274780242b5a7bc9c08624d0df8d228fee
Raquel Belfort <raquelzinha.belfort@gmail.com>
2020-09-07 18:31:36 -0300

    Adição de resolução.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace nomecompleto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("Olá, usuário! Por favor, escreva seu primeiro nome: ");
+            string nomeusuario = Console.ReadLine();
+            Console.WriteLine("Agora, escreva seu sobrenome:");
+            string sobrenomeusuario = Console.ReadLine();
+            Console.Write("Seu nome completo é: " );
+            Console.WriteLine(nomeusuario + " " + sobrenomeusuario);
+            Console.Write("Seu nome de catálogo é: ");
+            Console.WriteLine(sobrenomeusuario.ToUpper() + ", " + nomeusuario);
+        }
+    }
+}
